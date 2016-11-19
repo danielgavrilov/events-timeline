7d5c3fad88a2762b4ed613a62845f0c8eff327ed
Daniel Gavrilov <dgavrilov95@gmail.com>
2016-11-19 21:56:40 +0000

    Fix order of animation

--- public/src/main.js
+++ public/src/main.js
@@ -78,7 +78,7 @@ function categoryAccessor(d) {
 
 getAllEvents().then((nodes) => {
 
-  nodes = nodes.sort((a,b) => d3.ascending(a.start_time, b.start_time));
+  nodes = nodes.sort((a,b) => d3.descending(a.start_time, b.start_time));
 
   // Colours
   // ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"]
