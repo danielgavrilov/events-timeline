efed5313443d5df8345ed8c8698d201ea708c12d
Daniel Gavrilov <dgavrilov95@gmail.com>
2016-11-26 10:06:01 +0000

    Update Facebook access token

--- public/src/facebook.js
+++ public/src/facebook.js
@@ -1,7 +1,7 @@
 import _ from "lodash";
 import axios from "axios";
 
-const ACCESS_TOKEN = "EAAK3kpvZBViIBAMxJJu8mMPFgzLXtCT0rRy2oxxWTqPEB0sW6PKNAZCduqkR4J7huDkWwKqKhOC3mkftP2sAL4Lc1Wk2Y0U35T4ulQAcBn8abHv6HNJMBagBJfrMKecYrhXofbfIohZB5MPZCefpipYZB4dVvEQMZD";
+const ACCESS_TOKEN = "EAAK3kpvZBViIBAKZBRnqwBEkoN07Oi6sUJ0ZBXAQZCYHP9ZATFgvZBHgNzu3EEaG3ZA65GZC0UuYYEuvZAcajsZCZA80zT3xOku30T6f7PltjNtBnry2VEZAA9WVCFV9XMcaletjXTVn6h718XQb2QVSydwTe9VvcZBJvmn4ZD";
 
 const defaults = {
   access_token: ACCESS_TOKEN,
