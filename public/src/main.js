b767d062e7e12f304a5721f97df52cd65a0b4339
Daniel Gavrilov <dgavrilov95@gmail.com>
2016-11-26 10:26:58 +0000

    Do not manually assign colours

--- public/src/main.js
+++ public/src/main.js
@@ -80,39 +80,8 @@ getAllEvents().then((nodes) => {
 
   nodes = nodes.sort((a,b) => d3.descending(a.start_time, b.start_time));
 
-  // Colours
-  // ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"]
-
-  // ["#1f77b4", "#aec7e8", "#ff7f0e", "#ffbb78", "#2ca02c", "#98df8a", "#d62728", "#ff9896", "#9467bd", "#c5b0d5",
-  //  "#8c564b", "#c49c94", "#e377c2", "#f7b6d2", "#7f7f7f", "#c7c7c7", "#bcbd22", "#dbdb8d", "#17becf", "#9edae5"]
-
-  // ["#393b79", "#5254a3", "#6b6ecf", "#9c9ede", "#637939", "#8ca252", "#b5cf6b", "#cedb9c", "#8c6d31", "#bd9e39",
-  //  "#e7ba52", "#e7cb94", "#843c39", "#ad494a", "#d6616b", "#e7969c", "#7b4173", "#a55194", "#ce6dbd", "#de9ed6"]
-
-  // ["#3182bd", "#6baed6", "#9ecae1", "#c6dbef", "#e6550d", "#fd8d3c", "#fdae6b", "#fdd0a2", "#31a354", "#74c476",
-  //  "#a1d99b", "#c7e9c0", "#756bb1", "#9e9ac8", "#bcbddc", "#dadaeb", "#636363", "#969696", "#bdbdbd", "#d9d9d9"]
-
-  var presetColors = [
-    ["Hackathon", "#a55194"],
-    ["Tech Talk", "#d62728"],
-    ["Social", "#ff7f0e"],
-    ["Machine Learning", "#e0ac32"],
-    ["Data Visualization", "#17becf"],
-    ["Algorithms in C", "#756bb1"],
-    ["Coding & Cookies", "#e6550d"],
-    ["Blender & Brownies", "#8c564b"],
-    ["Hardware & Chill", "#e377c2"],
-    ["Coding Interview Prep", "#7f7f7f"],
-    ["Web Project", "#0f6b0f"],
-    ["VR Project", "#1b881b"],
-    ["IoT Project", "#3baf3b"],
-    ["Android Project", "#50c850"],
-    ["Other", "#1f77b4"]
-  ]
-
-  let color = d3.scale.ordinal()
-      .domain(presetColors.map(_.first))
-      .range(presetColors.map(_.last));
+  let color = d3.scale.category10()
+      .domain(nodes.map(d => d.label));
 
   let eventsPerCategory = d3.nest()
       .key(categoryAccessor)
